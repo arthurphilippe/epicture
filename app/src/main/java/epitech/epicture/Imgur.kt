f4cd1106162308ebea7bc98842ed3e3c3131b972
Arthur Philippe <arthur.philippe@epitech.eu>
2018-11-07 09:40:14 +0100

    managed to parse a json object into a kotlin object

--- app/src/main/java/epitech/epicture/Imgur.kt
+++ app/src/main/java/epitech/epicture/Imgur.kt
@@ -4,16 +4,24 @@ import android.os.AsyncTask.execute
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import org.json.JSONObject
 
 
 class Imgur {
+    data class Item(val id: String, val title: String, val favorite : Boolean, val link: String)
+
     companion object {
+        val gson = GsonBuilder().setPrettyPrinting().create()
+
         var loggedIn: Boolean = false
         var username: String? = null
         var accessToken: String? = null
         var refreshToken: String? = null
 
-        fun getSelfImages(): String? {
+        fun getSelfImages(): List<Item> {
             val client = OkHttpClient()
 
             val request = Request.Builder()
@@ -24,8 +32,11 @@ class Imgur {
                 .build()
 
             val response = client.newCall(request).execute()
-            val json = response.body()
-            return json?.string()
+            val json = response.body()?.string()
+            val jsonObj = JSONObject(json?.substring(json.indexOf("{"), json.lastIndexOf("}") + 1))
+            val data = jsonObj.getJSONArray("data")
+            var items : List<Item> = gson.fromJson(data.toString(), object : TypeToken<List<Item>>() {}.type)
+            return items
         }
     }
 }
